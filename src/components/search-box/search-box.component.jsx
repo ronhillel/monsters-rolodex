a90c299325c1d2cc2a9a048aaa612a36b676567f
Ron Hillel <drak372@gmail.com>
2021-04-08 14:28:06 +0300

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import './search-box.styles.css';
+
+export const SearchBox = ({
+    placeholder,
+    onChange
+}) => (
+    <input
+        className='search'
+        type='search'
+        placeholder={placeholder}
+        onChange={onChange}
+    />
+);
